dfcf1a11c7b0923655d21bc9f6893b2ea8ac4c81
francescacutrino <fcutrino@studenti.apuliadigitalmaker.it>
2022-04-06 09:11:43 +0200

    primo commit

---
+++ SpringWebGestioneProdotti/src/main/java/it/devlecce/SpringWebGestioneProdotti/SpringWebGestioneProdottiApplication.java
@@ -0,0 +1,13 @@
+package it.devlecce.SpringWebGestioneProdotti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebGestioneProdottiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebGestioneProdottiApplication.class, args);
+	}
+
+}
---
+++ SpringWebGestioneProdotti/src/test/java/it/devlecce/SpringWebGestioneProdotti/SpringWebGestioneProdottiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.devlecce.SpringWebGestioneProdotti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebGestioneProdottiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
